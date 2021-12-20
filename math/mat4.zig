53c908d8bf3f34ab7183e3a9eed587637a96a7ef
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-12-20 14:04:41 -0500

    added inverse

--- math/mat4.zig
+++ math/mat4.zig
@@ -9,6 +9,7 @@ pub const Mat4 = struct {
 
     const Self = @This();
 
+    /// returns an identity matrix
     pub fn identity() Self {
         return .{
             .m = .{
@@ -24,9 +25,10 @@ pub const Mat4 = struct {
         };
     }
 
+    /// multipliplies two matricies together
     pub fn mul(l: Self, r: Self) Self {
         // matrix to return
-        var mat = Self.idenity();
+        var mat = Self.identity();
         // for keeping track of the current op
         var row: usize = 0;
 
@@ -40,10 +42,65 @@ pub const Mat4 = struct {
                     sum += l.m[row][cur] * r.m[cur][col];
                 }
 
-                mat[row][col] = sum;
+                mat.m[row][col] = sum;
             }
         }
 
+        return mat;
+    }
+
+    /// inverse of a matrix
+    pub fn inv(mat: Self) Self {
+        // matrix to return
+        var inv_mat = Self.identity();
+
+        // determinant of a = |a|
+        // cofactor of a is c
+        // a^-1 = 1/|a| c^T
+
+        var s: [6]f32 = undefined;
+        var c: [6]f32 = undefined;
+
+        s[0] = mat.m[0][0] * mat.m[1][1] - mat.m[1][0] * mat.m[0][1];
+        s[1] = mat.m[0][0] * mat.m[1][2] - mat.m[1][0] * mat.m[0][2];
+        s[2] = mat.m[0][0] * mat.m[1][3] - mat.m[1][0] * mat.m[0][3];
+        s[3] = mat.m[0][1] * mat.m[1][2] - mat.m[1][1] * mat.m[0][2];
+        s[4] = mat.m[0][1] * mat.m[1][3] - mat.m[1][1] * mat.m[0][3];
+        s[5] = mat.m[0][2] * mat.m[1][3] - mat.m[1][2] * mat.m[0][3];
+
+        c[0] = mat.m[2][0] * mat.m[3][1] - mat.m[3][0] * mat.m[2][1];
+        c[1] = mat.m[2][0] * mat.m[3][2] - mat.m[3][0] * mat.m[2][2];
+        c[2] = mat.m[2][0] * mat.m[3][3] - mat.m[3][0] * mat.m[2][3];
+        c[3] = mat.m[2][1] * mat.m[3][2] - mat.m[3][1] * mat.m[2][2];
+        c[4] = mat.m[2][1] * mat.m[3][3] - mat.m[3][1] * mat.m[2][3];
+        c[5] = mat.m[2][2] * mat.m[3][3] - mat.m[3][2] * mat.m[2][3];
+
+        const determ = 1.0 / (s[0] * c[5] - s[1] * c[4] + s[2] * c[3] + s[3] * c[2] - s[4] * c[1] + s[5] * c[0]);
+
+        inv_mat.m[0][0] = (mat.m[1][1] * c[5] - mat.m[1][2] * c[4] + mat.m[1][3] * c[3]) * determ;
+        inv_mat.m[0][1] = (-mat.m[0][1] * c[5] + mat.m[0][2] * c[4] - mat.m[0][3] * c[3]) * determ;
+        inv_mat.m[0][2] = (mat.m[3][1] * s[5] - mat.m[3][2] * s[4] + mat.m[3][3] * s[3]) * determ;
+        inv_mat.m[0][3] = (-mat.m[2][1] * s[5] + mat.m[2][2] * s[4] - mat.m[2][3] * s[3]) * determ;
+        inv_mat.m[1][0] = (-mat.m[1][0] * c[5] + mat.m[1][2] * c[2] - mat.m[1][3] * c[1]) * determ;
+        inv_mat.m[1][1] = (mat.m[0][0] * c[5] - mat.m[0][2] * c[2] + mat.m[0][3] * c[1]) * determ;
+        inv_mat.m[1][2] = (-mat.m[3][0] * s[5] + mat.m[3][2] * s[2] - mat.m[3][3] * s[1]) * determ;
+        inv_mat.m[1][3] = (mat.m[2][0] * s[5] - mat.m[2][2] * s[2] + mat.m[2][3] * s[1]) * determ;
+        inv_mat.m[2][0] = (mat.m[1][0] * c[4] - mat.m[1][1] * c[2] + mat.m[1][3] * c[0]) * determ;
+        inv_mat.m[2][1] = (-mat.m[0][0] * c[4] + mat.m[0][1] * c[2] - mat.m[0][3] * c[0]) * determ;
+        inv_mat.m[2][2] = (mat.m[3][0] * s[4] - mat.m[3][1] * s[2] + mat.m[3][3] * s[0]) * determ;
+        inv_mat.m[2][3] = (-mat.m[2][0] * s[4] + mat.m[2][1] * s[2] - mat.m[2][3] * s[0]) * determ;
+        inv_mat.m[3][0] = (-mat.m[1][0] * c[3] + mat.m[1][1] * c[1] - mat.m[1][2] * c[0]) * determ;
+        inv_mat.m[3][1] = (mat.m[0][0] * c[3] - mat.m[0][1] * c[1] + mat.m[0][2] * c[0]) * determ;
+        inv_mat.m[3][2] = (-mat.m[3][0] * s[3] + mat.m[3][1] * s[1] - mat.m[3][2] * s[0]) * determ;
+        inv_mat.m[3][3] = (mat.m[2][0] * s[3] - mat.m[2][1] * s[1] + mat.m[2][2] * s[0]) * determ;
+
+        return inv_mat;
+    }
+
+    /// transposes a matrix
+    pub fn trans(m: Self) Self {
+        // matrix to return
+        var mat = Self.identity();
         return mat;
     }
 };
@@ -58,15 +115,97 @@ test "init" {
     });
 }
 
-test "init" {
+test "mul" {
     var i = Mat4.identity();
     var b = Mat4{ .m = .{
-        .{ 1, 2, 3, 0 },
+        .{ 1, 0, 0, 5 },
+        .{ 0, 1, 0, 3 },
+        .{ 0, 0, 1, 2 },
+        .{ 0, 0, 0, 1 },
+    } };
+    var a = Mat4{ .m = .{
+        .{ 1, 0, 0, 3 },
         .{ 0, 1, 0, 0 },
         .{ 0, 0, 1, 0 },
         .{ 0, 0, 0, 1 },
     } };
+    // rotate 90 degrees
+    var c = Mat4{ .m = .{
+        .{ 1, 0, 0, 0 },
+        .{ 0, -0.5, 0, 0 },
+        .{ 0, 0, -0.5, 0 },
+        .{ 0, 0, 0, 1 },
+    } };
 
+    // any multiplication with the identity is the same matrix
     try testing.expectEqual(i.mul(b), b);
     try testing.expectEqual(b.mul(i), b);
+
+    // try with a different one
+    try testing.expectEqual(a.mul(b).m, .{
+        .{ 1, 0, 0, 8 },
+        .{ 0, 1, 0, 3 },
+        .{ 0, 0, 1, 2 },
+        .{ 0, 0, 0, 1 },
+    });
+
+    // anotha one
+    try testing.expectEqual(b.mul(c).m, .{
+        .{ 1, 0, 0, 5 },
+        .{ 0, -0.5, 0, 3 },
+        .{ 0, 0, -0.5, 2 },
+        .{ 0, 0, 0, 1 },
+    });
+}
+
+test "inv" {
+    var i = Mat4.identity();
+    var a = Mat4{ .m = .{
+        .{ 1, 0, 0, 5 },
+        .{ 0, 1, 0, 3 },
+        .{ 0, 0, 1, 2 },
+        .{ 0, 0, 0, 1 },
+    } };
+
+    var b = Mat4{ .m = .{
+        .{ 5, 0, 0, 10 },
+        .{ 0, 0.5, 0, 6 },
+        .{ 0, 0, 0.5, 4 },
+        .{ 0, 0, 0, 5 },
+    } };
+
+    try testing.expectEqual(a.inv().m, .{
+        .{ 1, 0, 0, -5 },
+        .{ 0, 1, 0, -3 },
+        .{ 0, 0, 1, -2 },
+        .{ 0, 0, 0, 1 },
+    });
+
+    try testing.expectEqual(i.inv(), i);
+
+    //.{
+    //    .{ 0.2, 0, 0, -0.4 },
+    //    .{ 0, 2, 0, -2.4 },
+    //    .{ 0, 0, 2, -1.6 },
+    //    .{ 0, 0, 0, 0.2 },
+    //};
+    try testing.expectApproxEqAbs(b.inv().m[0][0], 0.2, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[0][1], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[0][2], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[0][3], -0.4, 0.001);
+
+    try testing.expectApproxEqAbs(b.inv().m[1][0], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[1][1], 2, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[1][2], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[1][3], -2.4, 0.001);
+
+    try testing.expectApproxEqAbs(b.inv().m[2][0], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[2][1], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[2][2], 2, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[2][3], -1.6, 0.001);
+
+    try testing.expectApproxEqAbs(b.inv().m[3][0], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[3][1], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[3][2], 0, 0.001);
+    try testing.expectApproxEqAbs(b.inv().m[3][3], 0.2, 0.001);
 }
