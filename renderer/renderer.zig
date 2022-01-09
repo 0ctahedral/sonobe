78ee43191710fe490bd96951d4ea8dea4f10c3b2
Morgan Rosenkranz <morgan@alloyenterprises.xyz>
2022-01-09 17:28:26 -0500

    added debug messenger callback

--- renderer/renderer.zig
+++ renderer/renderer.zig
@@ -9,8 +9,9 @@ const BaseDispatch = vk.BaseWrapper(&.{
 });
 
 const InstanceDispatch = vk.InstanceWrapper(&.{
-    .createDebugUtilsMessengerEXT,
     .destroyInstance,
+    .createDebugUtilsMessengerEXT,
+    .destroyDebugUtilsMessengerEXT,
     .createDevice,
     .destroySurfaceKHR,
     .enumeratePhysicalDevices,
@@ -89,11 +90,15 @@ const required_exts = [_][*:0]const u8{
     },
 };
 
+// TODO: set this in a config
+const required_layers = [_][*:0]const u8{"VK_LAYER_KHRONOS_validation"};
+
 var vkb: BaseDispatch = undefined;
 var vki: InstanceDispatch = undefined;
 var vkd: DeviceDispatch = undefined;
 
 var instance: vk.Instance = undefined;
+var messenger: vk.DebugUtilsMessengerEXT = undefined;
 
 const Self = @This();
 
@@ -117,12 +122,14 @@ pub fn init(allocator: Allocator, app_name: [*:0]const u8, window: glfw.Window) 
         .api_version = vk.API_VERSION_1_2,
     };
 
+    // TODO: query validation layers
+
     // create an instance
     instance = try vkb.createInstance(&.{
         .flags = .{},
         .p_application_info = &app_info,
-        .enabled_layer_count = 0,
-        .pp_enabled_layer_names = undefined,
+        .enabled_layer_count = required_layers.len,
+        .pp_enabled_layer_names = &required_layers,
         .enabled_extension_count = @intCast(u32, required_exts.len),
         .pp_enabled_extension_names = @ptrCast([*]const [*:0]const u8, &required_exts),
     }, null);
@@ -131,13 +138,51 @@ pub fn init(allocator: Allocator, app_name: [*:0]const u8, window: glfw.Window) 
     vki = try InstanceDispatch.load(instance, vk_proc);
     errdefer vki.destroyInstance(instance, null);
 
+    // setup debug msg
+    messenger = try vki.createDebugUtilsMessengerEXT(
+        instance,
+        &.{
+            .message_severity = .{
+                .warning_bit_ext = true,
+                .error_bit_ext = true,
+                .info_bit_ext = true,
+                //.verbose_bit_ext = true,
+            },
+            .message_type = .{
+                .general_bit_ext = true,
+                .validation_bit_ext = true,
+                .performance_bit_ext = true,
+            },
+            .pfn_user_callback = vk_debug,
+            .flags = .{},
+            .p_user_data = null,
+        },
+        null,
+    );
+
     // create a device
     // load dispatch functions which require device
+}
+
+fn vk_debug(
+    message_severity: vk.DebugUtilsMessageSeverityFlagsEXT.IntType,
+    message_types: vk.DebugUtilsMessageTypeFlagsEXT.IntType,
+    p_callback_data: ?*const vk.DebugUtilsMessengerCallbackDataEXT,
+    p_user_data: ?*anyopaque,
+) callconv(vk.vulkan_call_conv) vk.Bool32 {
+    _ = message_severity;
+    _ = message_types;
+    _ = p_callback_data;
+    _ = p_user_data;
+    std.log.info("{s}", .{p_callback_data.?.*.p_message});
+    return 1;
 }
 
 // shutdown the renderer
 pub fn deinit() void {
     //vkd.destroyDevice(device, null);
     //vki.destroySurfaceKHR(instance, surface, null);
+
+    vki.destroyDebugUtilsMessengerEXT(instance, messenger, null);
     vki.destroyInstance(instance, null);
 }
