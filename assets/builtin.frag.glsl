7eac3fa45709250b7301df79d94646357d2712ba
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2022-02-04 19:08:44 -0500

    new shaders who dis

--- assets/builtin.frag.glsl
+++ assets/builtin.frag.glsl
@@ -1,8 +1,10 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) out vec4 out_color;
+layout(location = 0) in vec3 v_color;
+
+layout(location = 0) out vec4 o_color;
 
 void main() {
-    out_color = vec4(1.0);
+    o_color = vec4(v_color, 1.0);
 }
--- assets/builtin.vert.glsl
+++ assets/builtin.vert.glsl
@@ -1,8 +1,12 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) in vec3 in_position;
+layout(location = 0) in vec3 a_pos;
+layout(location = 1) in vec3 a_color;
+
+layout(location = 0) out vec3 v_color;
 
 void main() {
-    gl_Position = vec4(in_position, 1.0);    
+    gl_Position = vec4(a_pos, 1.0);    
+    v_color = a_color;
 }
