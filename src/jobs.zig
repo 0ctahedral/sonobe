57a92858431981d1580f74e63c9cbbee92172503
morgan rosenkranz <rosenkranz1999@gmail.com>
2022-02-25 13:06:06 -0500

    jobs switching back to anyframe queues

--- src/jobs.zig
+++ src/jobs.zig
@@ -2,11 +2,9 @@ const std = @import("std");
 const expect = std.testing.expect;
 const Ringbuffer = @import("containers.zig").Ringbuffer;
 
-var saved_frame: anyframe = undefined;
-
 /// A job that is submitted to be run by a worker thread
 const Job = struct {
-    taskfn: fn ()  callconv(.Async) void = undefined,
+    taskfn: fn () callconv(.Async) void = undefined,
     fn task(self: @This(), data: []align(8) u8) void {
         _ = @asyncCall(data, undefined, self.taskfn, .{});
     }
@@ -40,7 +38,7 @@ const ShelvedJob = struct {
     frame: anyframe,
 };
 
-var job_queue = Ringbuffer(Job, 10).init();
+var job_queue = Ringbuffer(anyframe, 10).init();
 var wait_queue = Ringbuffer(ShelvedJob, 10).init();
 
 fn wait(counter: *Counter) void {
@@ -53,14 +51,16 @@ fn wait(counter: *Counter) void {
     }
 }
 
-//fn run(job: Job, data: [] align(8) u8) void {
-fn run(job: Job) void {
+fn run(job: Job, data: []align(8) u8) void {
     // add to job queue
-    job_queue.push(job) catch unreachable;
+    suspend {
+        job_queue.push(@frame()) catch unreachable;
+    }
+    _ = async job.task(data);
 }
 
 var a: u32 = 0;
-var c: Counter = Counter{.value = 1};
+var c: Counter = Counter{ .value = 1 };
 fn onesuspend() void {
     a += 1;
     std.debug.print("added 1\n", .{});
@@ -71,9 +71,10 @@ fn onesuspend() void {
 
 var done = false;
 
-fn loop(data: [] align(8) u8) void {
+fn loop() void {
     while (!done) {
-        // first check if there is anything sleeping to wake up
+        var tmp_queue = Ringbuffer(ShelvedJob, 10).init();
+
         while (wait_queue.pop()) |shelved| {
             // pop and check if the condition is met
             // if it is then we can run it
@@ -81,37 +82,45 @@ fn loop(data: [] align(8) u8) void {
             // for now there are no conditions
             if (shelved.counter.is_zero()) {
                 std.debug.print("resuming frame\n", .{});
-                resume shelved.frame;
+                // the frame is ready so we should push it to the
+                // front of the job_queue
+                // TODO: push front
+                job_queue.push(shelved.frame) catch unreachable;
             } else {
-                //std.debug.print("waiting for condition\n", .{});
-                wait_queue.push(shelved) catch unreachable;
-                // TODO: figure out way that we can only go through queue once 
-                break;
+                tmp_queue.push(shelved) catch {
+                    wait_queue.push(shelved) catch unreachable;
+                    break;
+                };
             }
+        }
+        // put them back
+        // TODO: is this the best way?
+        while (tmp_queue.pop()) |j| {
+            wait_queue.push(j) catch unreachable;
         }
 
         // then see if there are any new jobs to run
         while (job_queue.pop()) |job| {
             std.debug.print("starting job\n", .{});
-            _ = async job.task(data);
+            resume job;
         }
     }
 }
 
 test "queue while loop in thread" {
     a = 0;
-    var data: [4096] u8 align(8) = undefined;
+    var data: [4096]u8 align(8) = undefined;
     try expect(job_queue.len == 0);
 
     _ = async run(.{
         .taskfn = onesuspend,
-    });
+    }, data[0..]);
 
     try expect(a == 0);
 
     try expect(job_queue.len == 1);
 
-    var t = try std.Thread.spawn(.{}, loop, .{data[0..]});
+    var t = try std.Thread.spawn(.{}, loop, .{});
 
     std.time.sleep(1 * std.time.ns_per_s);
 
@@ -120,9 +129,8 @@ test "queue while loop in thread" {
     std.time.sleep(1 * std.time.ns_per_s);
 
     done = true;
-    
-    t.join();
 
+    t.join();
 
     try expect(job_queue.len == 0);
     try expect(a == 2);
