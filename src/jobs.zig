dc7c6b4514981f3692a2c9e91fc78003c164db5e
Morgan Rosenkranz <morgan@alloyenterprises.xyz>
2022-02-26 16:12:56 -0500

    jobs can now be any function with any args

--- src/jobs.zig
+++ src/jobs.zig
@@ -2,19 +2,6 @@ const std = @import("std");
 const expect = std.testing.expect;
 const Ringbuffer = @import("containers.zig").Ringbuffer;
 
-/// A job that is submitted to be run by a worker thread
-const Job = struct {
-    taskfn: fn () callconv(.Async) void = undefined,
-    counter: *Counter = undefined,
-
-    fn task(self: @This()) void {
-        var stack: [4096]u8 align(8) = undefined;
-        var f = @asyncCall(&stack, undefined, self.taskfn, .{});
-        await f;
-        self.counter.dec();
-    }
-};
-
 /// Basically a semaphore, indicates when a job dependency is done
 const Counter = struct {
     /// value stored in this counter
@@ -36,19 +23,17 @@ const Counter = struct {
 };
 
 /// A job that is waiting on a counter
-const ShelvedJob = struct {
+const WaitingFrame = struct {
     /// counter who's condition we must wait on
     counter: *Counter,
     /// function to resume from
     frame: anyframe,
 };
 
-/// jobs that have not started yet
-var job_queue = Ringbuffer(Job, 10).init();
 /// fibers that are ready
 var frame_queue = Ringbuffer(anyframe, 10).init();
 /// fibers that are waiting
-var wait_queue = Ringbuffer(ShelvedJob, 10).init();
+var wait_queue = Ringbuffer(WaitingFrame, 10).init();
 
 //fn wait(counter: *Counter, value?) void {
 fn wait(counter: *Counter) void {
@@ -61,28 +46,34 @@ fn wait(counter: *Counter) void {
     }
 }
 
-fn run(job: Job) void {
-    job_queue.push(job) catch unreachable;
+fn run(func: anytype, args: anytype, counter: *Counter) void {
+    suspend {
+        frame_queue.push(@frame()) catch unreachable;
+    }
+
+    // need to change the type to be async
+    comptime var T = @typeInfo(@TypeOf(func));
+    T.Fn.calling_convention = .Async;
+    const f = @ptrCast(@Type(T), func);
+
+    var stack: [4096]u8 align(8) = undefined;
+    await @asyncCall(&stack, undefined, f, args);
+    counter.dec();
 }
 
-var a: u32 = 0;
-var wait_c: Counter = Counter{ .value = 1 };
-fn onesuspend() void {
-    a += 1;
+fn onesuspend(a: *u32, c: *Counter) void {
+    a.* += 1;
     std.debug.print("added 1\n", .{});
-    wait(&wait_c);
-    a += 1;
+    wait(c);
+    a.* += 1;
     std.debug.print("added 1 again\n", .{});
 }
 
 var done = false;
 
-//threadlocal var thread_number: u32 = 0;
-
 fn loop(tn: u32) void {
-    //thread_number = tn;
     while (!done) {
-        var tmp_queue = Ringbuffer(ShelvedJob, 10).init();
+        var tmp_queue = Ringbuffer(WaitingFrame, 10).init();
 
         while (wait_queue.pop()) |shelved| {
             // pop and check if the condition is met
@@ -100,39 +91,31 @@ fn loop(tn: u32) void {
                 };
             }
         }
+
         // put them back
-        // TODO: is this the best way?
         while (tmp_queue.pop()) |j| {
             wait_queue.push(j) catch unreachable;
         }
 
         // resume from a frame
         while (frame_queue.pop()) |frame| {
-            std.debug.print("resuming job in {d}\n", .{tn});
             resume frame;
-        }
-
-        // then see if there are any new jobs to run
-        while (job_queue.pop()) |job| {
-            std.debug.print("starting job {d}\n", .{tn});
-            _ = async job.task();
         }
     }
 }
 
 test "queue while loop in thread" {
-    a = 0;
+    var a: u32 = 0;
+    var wait_c: Counter = Counter{ .value = 1 };
 
     var job_c = Counter{ .value = 1 };
 
-    run(.{
-        .counter = &job_c,
-        .taskfn = onesuspend,
-    });
+    _ = async run(onesuspend, .{ &a, &wait_c }, &job_c);
 
     try expect(a == 0);
-    //try expect(job_queue.len == 1);
     try expect(job_c.value == 1);
+
+    std.debug.print("job enqueued\n", .{});
 
     // TODO: lock to core
     var t = try std.Thread.spawn(.{}, loop, .{1});
