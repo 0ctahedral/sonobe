d63bcd46a546b929adbd93041bafdb86b6a03389
Morgan Rosenkranz <morgan@alloyenterprises.xyz>
2022-02-27 14:05:00 -0500

    changed coutner atomic order, and made run use a normal call like in the event loop

--- src/jobs.zig
+++ src/jobs.zig
@@ -2,23 +2,40 @@ const std = @import("std");
 const expect = std.testing.expect;
 const Ringbuffer = @import("containers.zig").Ringbuffer;
 
+pub const Jobs = @This();
+pub const num_threads = 4;
+pub const stack_size = 64 * 1024;
+
+var global: Jobs = undefined;
+pub var instance: ?*Jobs = null;
+
+/// state of the job system
+done: bool = false,
+
+worker_threads: [num_threads]std.Thread = undefined,
+
+/// fibers that are ready
+frame_queue: Ringbuffer(anyframe, 10),
+/// fibers that are waiting
+wait_queue: Ringbuffer(WaitingFrame, 10),
+
 /// Basically a semaphore, indicates when a job dependency is done
 const Counter = struct {
     /// value stored in this counter
-    value: usize,
+    value: usize = 0,
 
     const Self = @This();
 
     fn inc(self: *Self) void {
-        _ = @atomicRmw(usize, &self.value, .Add, 1, .Monotonic);
+        _ = @atomicRmw(usize, &self.value, .Add, 1, .SeqCst);
     }
 
     fn dec(self: *Self) void {
-        _ = @atomicRmw(usize, &self.value, .Sub, 1, .Monotonic);
+        _ = @atomicRmw(usize, &self.value, .Sub, 1, .SeqCst);
     }
 
-    fn is_zero(self: *Self) bool {
-        return @atomicLoad(usize, &self.value, .Monotonic) == 0;
+    fn val(self: *Self) usize {
+        return @atomicLoad(usize, &self.value, .SeqCst);
     }
 };
 
@@ -26,67 +43,95 @@ const Counter = struct {
 const WaitingFrame = struct {
     /// counter who's condition we must wait on
     counter: *Counter,
+    /// value the counter should be at to continue execution
+    condition: usize = 0,
     /// function to resume from
     frame: anyframe,
 };
 
-/// fibers that are ready
-var frame_queue = Ringbuffer(anyframe, 10).init();
-/// fibers that are waiting
-var wait_queue = Ringbuffer(WaitingFrame, 10).init();
+/// initialize the jobs subsystem
+/// this creates an instance
+pub fn init() !void {
+    global = Jobs{
+        .frame_queue = Ringbuffer(anyframe, 10).init(),
+        .wait_queue = Ringbuffer(WaitingFrame, 10).init(),
+    };
 
-//fn wait(counter: *Counter, value?) void {
-fn wait(counter: *Counter) void {
+    for (global.worker_threads) |*t, i| {
+        t.* = try std.Thread.spawn(.{}, loop, .{ &global, @intCast(u32, i) });
+    }
+
+    instance = &global;
+}
+
+/// shutdown the job subsystem
+/// joins all worker threads
+pub fn deinit() void {
+    global.done = true;
+    for (global.worker_threads) |t| {
+        t.join();
+    }
+    instance = null;
+}
+
+/// wait for a counter to reach a value
+pub fn wait(counter: *Counter, value: usize) void {
     suspend {
-        wait_queue.push(.{
+        instance.?.wait_queue.push(.{
             .frame = @frame(),
             .counter = counter,
+            .condition = value,
         }) catch unreachable;
         std.debug.print("frame rescheduled\n", .{});
     }
 }
 
-fn run(func: anytype, args: anytype, counter: *Counter) void {
-    suspend {
-        frame_queue.push(@frame()) catch unreachable;
-    }
+pub fn run(comptime func: anytype, args: anytype, counter: ?*Counter) !void {
+    const Wrapper = struct {
+        const Args = @TypeOf(args);
+        fn run(fnargs: Args, c: ?*Counter) void {
+            // yeild to put this on the queue
+            suspend {
+                if (c != null) {
+                    c.?.inc();
+                }
+                instance.?.frame_queue.push(@frame()) catch unreachable;
+            }
 
-    // need to change the type to be async
-    comptime var T = @typeInfo(@TypeOf(func));
-    T.Fn.calling_convention = .Async;
-    const f = @ptrCast(@Type(T), func);
+            @call(.{}, func, fnargs);
+            // cleanup
+            suspend {
+                if (c != null) {
+                    c.?.dec();
+                }
+            }
+        }
+    };
 
-    var stack: [4096]u8 align(8) = undefined;
-    await @asyncCall(&stack, undefined, f, args);
-    counter.dec();
+    std.debug.print("wrapper frame size: {d}\n", .{@sizeOf(@Frame(Wrapper.run))});
+
+    var stack: [stack_size]u8 align(8) = undefined;
+
+    var run_frame = @ptrCast(*@Frame(Wrapper.run), &stack);
+    run_frame.* = async Wrapper.run(args, counter);
 }
 
-fn onesuspend(a: *u32, c: *Counter) void {
-    a.* += 1;
-    std.debug.print("added 1\n", .{});
-    wait(c);
-    a.* += 1;
-    std.debug.print("added 1 again\n", .{});
-}
-
-var done = false;
-
-fn loop(tn: u32) void {
-    while (!done) {
+fn loop(self: *Jobs, tn: u32) void {
+    while (!self.done) {
         var tmp_queue = Ringbuffer(WaitingFrame, 10).init();
 
-        while (wait_queue.pop()) |shelved| {
+        while (self.wait_queue.pop()) |w| {
             // pop and check if the condition is met
             // if it is then we can run it
             // otherwise add to back of queue
             // for now there are no conditions
-            if (shelved.counter.is_zero()) {
+            if (w.counter.val() == w.condition) {
                 std.debug.print("resuming frame in {d}\n", .{tn});
                 // TODO: push front
-                frame_queue.push(shelved.frame) catch unreachable;
+                self.frame_queue.push(w.frame) catch unreachable;
             } else {
-                tmp_queue.push(shelved) catch {
-                    wait_queue.push(shelved) catch unreachable;
+                tmp_queue.push(w) catch {
+                    instance.?.wait_queue.push(w) catch unreachable;
                     break;
                 };
             }
@@ -94,31 +139,41 @@ fn loop(tn: u32) void {
 
         // put them back
         while (tmp_queue.pop()) |j| {
-            wait_queue.push(j) catch unreachable;
+            self.wait_queue.push(j) catch unreachable;
         }
 
         // resume from a frame
-        while (frame_queue.pop()) |frame| {
+        while (self.frame_queue.pop()) |frame| {
             resume frame;
         }
     }
 }
 
-test "queue while loop in thread" {
+// TESTS AND STUFF
+
+fn onesuspend(a: *u32, c: *Counter) void {
+    a.* += 1;
+    std.debug.print("added 1\n", .{});
+    wait(c, 0);
+    a.* += 1;
+    std.debug.print("added 1 again\n", .{});
+}
+
+test "function with single suspend" {
     var a: u32 = 0;
     var wait_c: Counter = Counter{ .value = 1 };
 
-    var job_c = Counter{ .value = 1 };
+    try init();
+    defer deinit();
 
-    _ = async run(onesuspend, .{ &a, &wait_c }, &job_c);
+    var job_c = Counter{};
+
+    try run(onesuspend, .{ &a, &wait_c }, &job_c);
 
     try expect(a == 0);
     try expect(job_c.value == 1);
 
     std.debug.print("job enqueued\n", .{});
-
-    // TODO: lock to core
-    var t = try std.Thread.spawn(.{}, loop, .{1});
 
     std.time.sleep(1 * std.time.ns_per_s);
 
@@ -129,11 +184,36 @@ test "queue while loop in thread" {
 
     std.time.sleep(1 * std.time.ns_per_s);
 
-    done = true;
-
-    t.join();
-
-    //try expect(job_queue.len == 0);
     try expect(a == 2);
     try expect(job_c.value == 0);
+}
+
+fn doStuff(v: *u32, t: u32) void {
+    var i: u32 = 0;
+    while (i < t) : (i += 1) {
+        v.* += 1;
+    }
+}
+
+test "add a bunch of jobs" {
+    try init();
+    defer deinit();
+
+    var x: u32 = 0;
+    var y: u32 = 0;
+    var z: u32 = 0;
+
+    var job_c = Counter{};
+
+    try run(doStuff, .{ &x, 100 }, &job_c);
+    try run(doStuff, .{ &y, 120 }, &job_c);
+    try run(doStuff, .{ &z, 10 }, &job_c);
+
+    // spin lock until all jobs are done
+    while (job_c.val() != 0) {}
+
+    try std.testing.expect(job_c.val() == 0);
+    try std.testing.expect(x == 100);
+    try std.testing.expect(y == 120);
+    try std.testing.expect(z == 10);
 }
