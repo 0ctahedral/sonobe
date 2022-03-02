88390f2c59d31916073844e7fcfc375b8f6a3e89
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2022-03-02 18:10:23 -0500

    updated freelist to use pointers, including anyopaque

--- src/containers/freelist.zig
+++ src/containers/freelist.zig
@@ -5,20 +5,27 @@ const Allocator = std.mem.Allocator;
 pub fn FreeList(
     comptime T: type,
 ) type {
+
+    // union type to store either the next free index or the itme
+    const item_t = extern union {
+        item: T,
+        next: u32,
+    };
+
     // TODO: assert that T is of greater or equal alignment
-    if (@sizeOf(T) < @sizeOf(u32)) @compileError("T must be at least as big as number");
+    if (@sizeOf(T) < @sizeOf(item_t)) @compileError("T must be at least as big as item storage type");
+
     return struct {
-        /// union type to store either the next free i
-        const U = extern union {
-            item: T,
-            next: u32,
-        };
+        const Item = item_t;
+
+        /// type of index elements
+        const Index = u32;
 
         const Self = @This();
 
         /// underlying storage
         /// head is the first element
-        mem: []U,
+        mem: []Item,
 
         allocator: Allocator,
 
@@ -26,32 +33,44 @@ pub fn FreeList(
         pub fn init(allocator: Allocator, size: usize) !Self {
             var self = Self{
                 .allocator = allocator,
-                .mem = try allocator.alloc(U, size),
+                .mem = try allocator.alloc(Item, size),
             };
-            for (self.mem) |*u| {
-                u.* = .{ .next = 0 };
+            // TODO: is this necessary?
+            for (self.mem) |*u, i| {
+                u.* = .{ .next = @intCast(u32, i) + 1 };
             }
             // set head
             self.mem[0] = .{ .next = 1 };
+            self.mem[size - 1] = .{ .next = 0 };
             return self;
         }
 
-        pub fn alloc(self: *Self) !*T {
-            var head_ptr = &self.mem[0];
-            // TODO: bounds checking and stuff
-            var ptr = &self.mem[head_ptr.next];
-            head_ptr.*.next = ptr.next;
-            ptr.* = .{ .item = undefined };
-            return &ptr.item;
+        pub inline fn allocAny(self: *Self) !*anyopaque {
+            @ptrCast(*anyopaque, self.alloc());
         }
 
-        pub fn free(self: *Self, item_ptr: *T) void {
-            // does this pointer exist?
-            const ptr = @ptrCast(*U, item_ptr);
-            // set the item
-            ptr.* = .{ .next = self.mem[0].next };
-            // TOOD: get index from pointer?
-            self.mem[0].next = 0;
+        pub fn alloc(self: *Self) !*T {
+            const slot = self.mem[0].next;
+            self.mem[0].next = self.mem[@as(usize, slot)].next;
+            // if the head is pointing to itself then the buffer is full
+            if (slot > 0) {
+                self.mem[slot] = .{ .item = undefined };
+                return &self.mem[slot].item;
+            }
+
+            // TODO: resize?
+            return error.OutOfMemory;
+        }
+
+        pub inline fn free(self: *Self, item_ptr: *T) void {
+            self.freeAny(item_ptr);
+        }
+
+        pub fn freeAny(self: *Self, item_ptr: *anyopaque) void {
+            const index = (@ptrToInt(item_ptr) - @ptrToInt(self.mem.ptr)) / @sizeOf(Item);
+            // insert at the front of the list
+            self.mem[index] = .{ .next = self.mem[0].next };
+            self.mem[0].next = @intCast(u32, index);
         }
 
         pub fn deinit(self: *Self) void {
@@ -60,27 +79,47 @@ pub fn FreeList(
     };
 }
 
+const foo = struct {
+    x: u64 = 0,
+    y: u32 = 0,
+};
+
 test "init" {
-    const foo = struct {
-        x: u64 = 0,
-        y: u32 = 0,
-    };
     const FooList = FreeList(foo);
 
     var fl = try FooList.init(std.testing.allocator, 100);
     defer fl.deinit();
 
-    try expect(@sizeOf(FooList.U) == @sizeOf(foo));
+    try expect(@sizeOf(FooList.Item) == @sizeOf(foo));
 }
 
 test "addressing" {
-    const foo = struct {
-        x: u64 = 0,
-    };
     const FooList = FreeList(foo);
-
     var fl = try FooList.init(std.testing.allocator, 100);
     defer fl.deinit();
-    const p = try fl.alloc();
-    try expect(@ptrToInt(p) == @ptrToInt(&fl.mem[1]));
+    const p1 = try fl.alloc();
+    try expect(@ptrToInt(p1) == @ptrToInt(&fl.mem[1]));
+    const p2 = try fl.alloc();
+    try expect(@ptrToInt(p2) == @ptrToInt(&fl.mem[2]));
+
+    fl.free(p2);
+    try expect(fl.mem[0].next == 2);
+}
+
+test "exceed memory" {
+    const FooList = FreeList(foo);
+    var fl = try FooList.init(std.testing.allocator, 100);
+    defer fl.deinit();
+
+    var i: u8 = 0;
+    while (i < 99) : (i += 1) {
+        _ = try fl.alloc();
+    }
+    try std.testing.expectError(error.OutOfMemory, fl.alloc());
+}
+
+test "array type" {
+    const StackList = FreeList([400]u8);
+    var fl = try StackList.init(std.testing.allocator, 10);
+    defer fl.deinit();
 }
