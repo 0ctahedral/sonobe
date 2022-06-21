9fa3a1f39d75801369bcb7fafec17ef7feada540
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2022-06-21 15:22:20 -0400

    added mouse position setting to input system

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ pub const mmath = @import("./math.zig");
 pub const Renderer = @import("renderer.zig");
 pub const Platform = @import("platform.zig");
 pub const Events = @import("events.zig");
+pub const Input = @import("input.zig");
 pub const Jobs = @import("jobs.zig");
 
 test {
