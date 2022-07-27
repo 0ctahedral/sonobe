8b43df4817c5ebeef809131cbe7d06590bc4dc55
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2022-07-27 16:19:25 -0400

    grabbed vec4 from other branch

--- src/math.zig
+++ src/math.zig
@@ -2,6 +2,7 @@
 pub const util = @import("math/util.zig");
 pub const Vec2 = @import("math/vec2.zig").Vec2;
 pub const Vec3 = @import("math/vec3.zig").Vec3;
+pub const Vec4 = @import("math/vec4.zig").Vec4;
 pub const Mat4 = @import("math/mat4.zig").Mat4;
 pub const Quat = @import("math/quat.zig").Quat;
 pub const Transform = @import("math/transform.zig").Transform;
