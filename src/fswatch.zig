509a34d48d396070970f02ea184504ec4325f7e7
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2022-09-01 18:07:45 -0400

    added handles to fswatch

--- src/fswatch.zig
+++ src/fswatch.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Handle = @import("handle.zig").Handle;
 const os = std.os;
 const sys = std.os.system;
 const Allocator = std.mem.Allocator;
@@ -35,7 +36,7 @@ pub const KQWatcher = struct {
         os.close(self.kq);
     }
 
-    pub fn addFile(self: *Self, path: []const u8) !usize {
+    pub fn addFile(self: *Self, path: []const u8) !Handle(.File) {
         self.mutex.lock();
         defer self.mutex.unlock();
 
@@ -48,13 +49,13 @@ pub const KQWatcher = struct {
 
         std.debug.print("adding file: {s}\n", .{path});
 
-        return self.datas.items.len - 1;
+        return Handle(.File){ .id = @intCast(u32, self.datas.items.len - 1) };
     }
 
     /// checks if a file is changed
     /// if it is then return true and reset file
-    pub fn checkChanged(self: *Self, idx: usize) bool {
-        var d = &self.datas.items[idx];
+    pub fn checkChanged(self: *Self, handle: Handle(.File)) bool {
+        var d = &self.datas.items[handle.id];
         const changed = d.changed.load(.Unordered);
         if (changed) {
             d.changed.store(false, .Unordered);
@@ -128,18 +129,18 @@ pub const KQWatcher = struct {
     }
 };
 
-fn changedloop(kqw: *KQWatcher, inds: []usize, paths: [][]const u8) void {
+fn changedloop(kqw: *KQWatcher, handles: []Handle(.File), paths: [][]const u8) void {
     while (true) {
         std.time.sleep(std.time.ns_per_s);
-        for (inds) |idx, i| {
-            if (kqw.checkChanged(idx)) {
+        for (handles) |h, i| {
+            if (kqw.checkChanged(h)) {
                 std.debug.print("{s} changed!\n", .{paths[i]});
             }
         }
     }
 }
 
-test "test" {
+test "interactive" {
     const allocator = std.testing.allocator;
     // this runs in own thread
     // given a file to watch and a counter, we can reset the counter when the file changes, remove it from the freelist
@@ -149,83 +150,15 @@ test "test" {
     defer kqw.deinit();
 
     var paths = [_][]const u8{ "test/file.txt", "test/bloop.txt", "test/bop.txt" };
-    var inds: [paths.len]usize = undefined;
+    var file_handles: [paths.len]Handle(.File) = undefined;
 
     var kthread = try std.Thread.spawn(.{}, KQWatcher.run, .{&kqw});
     for (paths) |p, i| {
-        inds[i] = try kqw.addFile(p);
+        file_handles[i] = try kqw.addFile(p);
     }
 
-    var cthread = try std.Thread.spawn(.{}, changedloop, .{ &kqw, &inds, &paths });
+    var cthread = try std.Thread.spawn(.{}, changedloop, .{ &kqw, &file_handles, &paths });
 
     cthread.join();
     kthread.join();
 }
-
-// fn readOut(file: std.fs.File, c: *Counter, allocator: std.mem.Allocator) void {
-//     // wait for c to equal zero
-//     jobs.wait(c, 0);
-//
-//     // read contents of file and print them
-//     const reader = file.reader();
-//     var buf = reader.readAllAlloc(allocator, 1024) catch {
-//         std.debug.print("could not read\n", .{});
-//         return;
-//     };
-//     defer allocator.free(buf);
-//     std.debug.print("contents: {s}\n", .{buf});
-// }
-//
-// fn printDone(c: *Counter) void {
-//     jobs.wait(c, 0);
-//     std.debug.print("woohoo\n", .{});
-// }
-// const test_tmp_dir = "tmp_test";
-// test "wait for file change" {
-//     const allocator = std.testing.allocator;
-//     try jobs.init(allocator);
-//     defer jobs.deinit();
-//
-//     var kqw = try KQWatcher.init(allocator);
-//     defer kqw.deinit();
-//
-//     // create directory and open file
-//     try std.fs.cwd().makePath(test_tmp_dir);
-//     defer std.fs.cwd().deleteTree(test_tmp_dir) catch {};
-//
-//     const file_path = try std.fs.path.join(allocator, &[_][]const u8{ test_tmp_dir, "file.txt" });
-//     defer allocator.free(file_path);
-//
-//     const contents =
-//         \\line 1
-//         \\line 2
-//     ;
-//     const contents2 =
-//         \\lorem
-//         \\ipsum
-//     ;
-//     try std.fs.cwd().writeFile(file_path, contents);
-//     // open the file
-//
-//     var file_c = Counter{};
-//     var thread = try std.Thread.spawn(.{}, KQWatcher.run, .{&kqw});
-//     try kqw.addFile(file_path, &file_c);
-//
-//     var i: u8 = 0;
-//     while (i < 3) : (i += 1) {
-//         var done = Counter{};
-//
-//         // jtry jobs.run(readOut, .{ file, &file_c, allocator }, &done);
-//         try jobs.run(printDone, .{&file_c}, &done);
-//
-//         try std.testing.expect(file_c.val() == 1);
-//         try std.fs.cwd().writeFile(file_path, contents2);
-//
-//         std.time.sleep(std.time.ns_per_s);
-//
-//         try std.testing.expect(file_c.val() == 0);
-//         try std.testing.expect(done.val() == 0);
-//     }
-//
-//     thread.join();
-// }
