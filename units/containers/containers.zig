2436cef85192d768bde52c7598ff9f02c1cbf39e
mjr <rosenkranz1999@gmail.com>
2022-10-27 09:26:20 -0400

    removed hash from containers as the std library has much better implementations

--- units/containers/containers.zig
+++ units/containers/containers.zig
@@ -2,7 +2,6 @@
 //! Will include hashmaps, arrays, and sets, and stuff
 //!
 
-pub const hash = @import("hash.zig");
 pub const RingBuffer = @import("ring_buffer.zig").RingBuffer;
 pub const FreeList = @import("free_list.zig").FreeList;
 pub const Cache = @import("cache.zig").Cache;
