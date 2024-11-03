4ac1dd2d3cdd36b95c9d0839d0d523d4c8ceb6b5
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2024-11-03 14:47:52 -0500

    starting from the entrypoint up

--- build.zig
+++ build.zig
@@ -1,71 +1,25 @@
 const std = @import("std");
-const builtin = @import("builtin");
-const Builder = std.build.Builder;
-pub const units = @import("units/units.zig");
 
-const glfw = @import("deps/mach-glfw/build.zig");
-// TODO: get rid of this
-const prefix = @import("units/platform/build.zig").vkprefix;
+pub fn build(b: *std.Build) void {
+    // set the target and optimization
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-pub fn build(b: *Builder) !void {
-    // build all the projects
-    const testbed = @import("testbed/build.zig");
-    try testbed.build(b);
+    // create executable
+    const exe = b.addExecutable(.{
+        .name = "sonobe",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const test_step = b.step("test", "run fs tests");
+    // install in the zig-out directory
+    b.installArtifact(exe);
 
-    // const fs_tests = b.addTest("units/fs/test.zig");
-    // fs_tests.addPackage(units.fs);
-    // fs_tests.addPackage(units.utils);
-    // test_step.dependOn(&fs_tests.step);
 
-    const utils_tests = b.addTest("testbed/imgui/tree.zig");
-    for (units.utils.dependencies.?) |d| {
-        utils_tests.addPackage(d);
-    }
-    test_step.dependOn(&utils_tests.step);
-}
-
-pub fn linkDeps(b: *Builder, exe: *std.build.LibExeObjStep) !void {
-    glfw.link(b, exe, .{});
-    try compileShadersInDir(b, "assets/shaders/", exe);
-}
-
-/// compile all shaders in a given directory 
-pub fn compileShadersInDir(b: *Builder, shader_path: []const u8, exe: *std.build.LibExeObjStep) !void {
-    var dir = try std.fs.cwd().openDir(shader_path, .{ .iterate = true });
-    var iter = dir.iterate();
-    while (try iter.next()) |e| {
-        if (e.kind == .File) {
-            const len = e.name.len;
-            if (len <= 10) continue;
-            const ext = e.name[(len - 10)..];
-            const file_name = e.name[0..(len - 10)];
-
-            var stage: []const u8 = "";
-
-            if (std.mem.eql(u8, ext, ".vert.glsl")) {
-                stage = "vert";
-            } else if (std.mem.eql(u8, ext, ".frag.glsl")) {
-                stage = "frag";
-            } else {
-                continue;
-            }
-
-            var buf1: [256]u8 = undefined;
-            var buf2: [256]u8 = undefined;
-
-            var cmd_str = [_][]const u8{
-                prefix ++ "/bin/glslc",
-                try std.fmt.bufPrint(&buf1, "-fshader-stage={s}", .{stage}),
-                try std.fs.path.join(b.allocator, &.{ shader_path, e.name }),
-                "-o",
-                try std.fmt.bufPrint(&buf2, "{s}/{s}.{s}.spv", .{ shader_path, file_name, stage }),
-            };
-
-            const compile_spv = b.addSystemCommand(&cmd_str);
-
-            exe.step.dependOn(&compile_spv.step);
-        }
-    }
+    // create command and step to run this
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
