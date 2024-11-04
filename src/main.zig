b1864ad03c7ff0c042754f2fc658575ac0abe2fb
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2024-11-04 16:33:10 -0500

    used event to capture sigint

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const core = @import("core/core.zig");
-const log = core.logger.Logger("main");
+
+const posix = std.posix;
+const SIG = posix.system.SIG;
 
 const Runtime = struct {
     const log = core.logger.Logger("runtime");
@@ -8,27 +10,34 @@ const Runtime = struct {
     const Self = @This();
 
     // fixed timestep in seconds
-    timestep: f32 = 0.01,
+    fixed_step_s: f32 = 0.01,
 
-    // accumulator for time elapsed between fixed timed steps
-    acc: f32 = 0,
+    // accumulator for time elapsed between fixed time steps
+    frame_acc_s: f32 = 0,
 
-    // frame time
-    // begin_frame: std.time.Instant,
+    // total time ellapsed since starting
+    ellapsed_ns: u64 = 0,
 
-    pub fn init() !Self {
-        return .{
-            // .begin_frame = try std.time.Instant.now(),
-        };
+    is_running: std.Thread.ResetEvent = .{},
+
+    pub fn init(self: *Self) !void {
+        log.debug("init runtime", .{});
+        self.ellapsed_ns = 0;
+        self.is_running.set();
     }
 
     pub fn deinit(self: *Self) void {
+        log.debug("deinit runtime", .{});
         // TODO: any cleanup we need
         _ = self;
     }
 
+    /// total time the runtime has been running, in seconds
+    fn time(self: Self) f32 {
+        return @as(f32, @floatFromInt(self.ellapsed_ns)) * 1e-9;
+    }
 
-    /// runs once every fixed timestep (16.6ms)
+    /// runs once every fixed timestep
     pub fn fixedUpdate(self: Self, dt: f32) void {
         _ = self;
         _ = dt;
@@ -42,11 +51,13 @@ const Runtime = struct {
 
     fn loop(self: *Self) !void {
         var begin_frame = try std.time.Instant.now();
-        while (true) {
+        while (self.is_running.isSet()) {
             const end_frame = try std.time.Instant.now();
 
             // delta time for this frame in nanoseconds
             const frame_ns = end_frame.since(begin_frame);
+            self.ellapsed_ns += frame_ns;
+
             // convert to seconds
             const frame_s = @as(f32, @floatFromInt(frame_ns)) * 1.0e-9;
 
@@ -54,11 +65,11 @@ const Runtime = struct {
             begin_frame = end_frame;
 
             // update the accumulator
-            self.acc += frame_s;
+            self.frame_acc_s += frame_s;
 
-            while (self.acc >= self.timestep) {
-                self.fixedUpdate(self.timestep);
-                self.acc -= self.timestep;
+            while (self.frame_acc_s >= self.fixed_step_s) {
+                self.fixedUpdate(self.fixed_step_s);
+                self.frame_acc_s -= self.fixed_step_s;
             }
 
             // while we still have time since the last frame, run update
@@ -67,14 +78,32 @@ const Runtime = struct {
     }
 };
 
-pub fn main() !void {
-    log.debug("starting main loop", .{});
 
-    var runtime = try Runtime.init();
+var running_event: ?*std.Thread.ResetEvent = null;
+
+fn handle_sig(sig: c_int) callconv(.C) void {
+    _ = sig;
+    if (running_event != null) {
+        running_event.?.reset();
+    }
+}
+
+pub fn main() !void {
+
+    var runtime = Runtime{};
+    running_event = &runtime.is_running;
+
+    const sigact = posix.Sigaction{
+        .handler = .{ .handler = handle_sig },
+        .mask = posix.empty_sigset,
+        .flags = 0,
+    };
+    // setup signal handlers
+    try std.posix.sigaction(SIG.INT, &sigact, null);
+
+    try runtime.init();
     defer runtime.deinit();
 
     // game loop help from: https://gafferongames.com/post/fix_your_timestep/
     try runtime.loop();
-
-    log.debug("ended main loop", .{});
 }
