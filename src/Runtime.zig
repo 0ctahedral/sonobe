b3193d0d2b5652128f21602e27e2c4d12d397e44
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2024-11-04 16:56:04 -0500

    moved signal handling out of main

--- src/Runtime.zig
+++ src/Runtime.zig
@@ -2,7 +2,15 @@ const core = @import("core/core.zig");
 const std = @import("std");
 const log = core.logger.Logger("runtime");
 
+// TODO: put this in the platform
+const posix = std.posix;
+const SIG = posix.system.SIG;
+
+
 const Runtime = @This();
+
+/// is the runtime in a running state?
+var is_running: std.Thread.ResetEvent = .{};
 
 // fixed timestep in seconds
 fixed_step_s: f32 = 0.01,
@@ -13,12 +21,30 @@ frame_acc_s: f32 = 0,
 // total time ellapsed since starting
 ellapsed_ns: u64 = 0,
 
-is_running: std.Thread.ResetEvent = .{},
 
 pub fn init(self: *Runtime) !void {
     log.debug("init runtime", .{});
     self.ellapsed_ns = 0;
-    self.is_running.set();
+    is_running.set();
+
+
+    // setup signal handlers
+
+    // basically a lambda to add a handler for sigint
+    // to turn off running
+    const handle = struct {
+        fn call(sig: c_int) callconv(.C) void {
+            _ = sig;
+            is_running.reset();
+        }
+
+    }.call;
+    const sigact = posix.Sigaction{
+        .handler = .{ .handler = handle },
+        .mask = posix.empty_sigset,
+        .flags = 0,
+    };
+    try std.posix.sigaction(SIG.INT, &sigact, null);
 }
 
 pub fn deinit(self: *Runtime) void {
@@ -47,7 +73,7 @@ pub fn update(self: Runtime, dt: f32) void {
 /// game inspired from from: https://gafferongames.com/post/fix_your_timestep/
 pub fn loop(self: *Runtime) !void {
     var begin_frame = try std.time.Instant.now();
-    while (self.is_running.isSet()) {
+    while (is_running.isSet()) {
         const end_frame = try std.time.Instant.now();
 
         // delta time for this frame in nanoseconds
--- src/main.zig
+++ src/main.zig
@@ -4,27 +4,9 @@ const Runtime = @import("Runtime.zig");
 const posix = std.posix;
 const SIG = posix.system.SIG;
 
-var running_event: ?*std.Thread.ResetEvent = null;
-
-fn handle_sig(sig: c_int) callconv(.C) void {
-    _ = sig;
-    if (running_event != null) {
-        running_event.?.reset();
-    }
-}
-
 pub fn main() !void {
 
     var runtime = Runtime{};
-    running_event = &runtime.is_running;
-
-    const sigact = posix.Sigaction{
-        .handler = .{ .handler = handle_sig },
-        .mask = posix.empty_sigset,
-        .flags = 0,
-    };
-    // setup signal handlers
-    try std.posix.sigaction(SIG.INT, &sigact, null);
 
     try runtime.init();
     defer runtime.deinit();
